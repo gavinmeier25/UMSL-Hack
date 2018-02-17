900dc319636563026b32acf8904515d6a3bf2bbf
lrb711 <lucasrbarth@gmail.com>
2018-02-16 21:56:06 -0600

    workInProgress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+
+const API_KEY = 'AIzaSyAMqWOtEahk1Ip5T7J_-45dcy-CuAZ3YE0';
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state ={
+    videos: [],
+    selectedVideo: null
+  };
+
+  this.videoSearch('USML');
+}
+
+videoSearch(term){
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  });
+}
   render() {
+    
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    
     return (
       <div className="App">
-        
-        <header className="API Gov">
-        </header>
-        
-        <body>
-        
-        </body>
+        <SearchBar onSearchTermChange = {videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
+        videos = {this.state.videos}
+        />
       </div>
     );
   }
