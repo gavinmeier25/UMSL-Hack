2852ddbe028101984311aca7cb944b3766a84abc
lrb711 <lucasrbarth@gmail.com>
2018-02-16 22:53:57 -0600

    WorkingYouTube

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -4,7 +4,7 @@ const VideoDetail = ({video}) => {
         return <div> Loading . . . </div>;
     }
 const videoId = video.id.videoId;
-const url = `https://wwww.youtube.com/embed/${videoId}`;
+const url = `https://www.youtube.com/embed/${videoId}`;
 
 return (
     <div className= "video-detail col-md-8">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+const API_KEY = 'AIzaSyDsKPmKpKAxONN9CnEDtldAKIeUfXXW9-o';
+
+
+
+
+class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state= {
+    videos: [],
+    selectedVideo: null
+  };
+
+  this.videoSearch('UMSL Hack');
+}
+
+  videoSearch(term){
+
+    YTSearch({key: API_KEY, term: term},(videos) =>{
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+
+
+  render(){
+
+  const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+  return(
+    <div>
+      <SearchBar onSearchTermChange ={videoSearch} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+       onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
+       videos = {this.state.videos} />
+    </div>
+  );
+}
+}
+
+
+
+
+ReactDOM.render(<App />, document.querySelector('.container') );
