f00d0a077959215075b1ec351f7865dcd82ee412
lrb711 <lucasrbarth@gmail.com>
2018-02-17 00:36:00 -0600

    Added styling for video to be in cetner

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -9,15 +9,24 @@ const url = `https://www.youtube.com/embed/${videoId}`;
 return (
     <div className= "video-detail">
         <div>
-        <iframe style={{paddingLeft:'460px'}} src= {url}> </iframe>
+        <iframe 
+        style={{    
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            position: 'absolute',
+            width: '300px',
+            height: '200px',
+            top: '30%',
+            left: '50%',
+            margin: '-100px 0 0 -150px',
+        }} 
+        src= {url}> </iframe>
         </div>
     
-        <div className= "details">
-        <div> {video.snippet.title}</div>
-        <div> {video.snippet.descripiting}</div>
-        </div>
     </div>
 
 );  
 };
 export default VideoDetail;
+//z-index: 15,
+//<iframe style={{paddingLeft:'460px'}} src= {url}> </iframe>
